737876a0e10298d93847039de7845d6b583c03c9
BrenoBar51 <brenobarcelos9@gmail.com>
2023-09-16 21:53:08 -0300

    ok

---
+++ testeAPI/src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <>
+            <h1 id="resposta" className="text-black font-bold">
+                TesteAPI
+            </h1>
+        </>
+    )
+}
